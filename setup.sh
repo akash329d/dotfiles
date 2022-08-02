304f5dc1756100c4556f24417c5202bc76096139
Akash D <akash329d@berkeley.edu>
2022-08-01 20:38:37 -0700

    fixes

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@
 source ~/.zplug/init.zsh
 
 # Let zplug manage itself
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+# zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
 # Dotfiles
-zplug "akash329d/dotfiles", use:"{zshrc,p10k}"
+zplug "akash329d/dotfiles", use:"{zshrc,p10k}", hook-build:'zplug install && zplug load'
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check; then
