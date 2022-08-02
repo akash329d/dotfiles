82984948a895f2034d8376bf4b2be83ec3b7438a
Akash D <akash329d@berkeley.edu>
2022-08-01 20:22:15 -0700

    remove plugins

--- setup.sh
+++ setup.sh
@@ -12,17 +12,6 @@ source ${SCRIPT_DIR}/zplug/init.zsh
 # Let zplug manage itself
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
-# Zsh Plugins
-zplug "romkatv/powerlevel10k", as:theme, depth:1
-zplug "zsh-users/zsh-autosuggestions"
-zplug "agkozak/zsh-z"
-zplug "Aloxaf/fzf-tab"
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
-
-# Binaries
-zplug "sharkdp/bat", as:command, from:gh-r, use:"*x86_64*musl*", rename-to:bat
-zplug "junegunn/fzf", as:command, from:gh-r
-
 # Dotfiles
 zplug "akash329d/dotfiles", at:dotfiles, use:"{zshrc,p10k}"
 
