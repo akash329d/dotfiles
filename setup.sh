67cf55d34759f16f9b27fd764b3afcc0069bdb30
Akash D <akash329d@berkeley.edu>
2022-08-01 21:09:13 -0700

    updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+set -e
+
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 source ~/.zplug/init.zsh
@@ -7,11 +9,13 @@ source ~/.zplug/init.zsh
 # Dotfiles
 zplug "akash329d/dotfiles", use:"{zshrc,p10k}"
 
-# Install my dotfiles
-zplug load --verbose
-
-# Install plugins if there are plugins that have not been installed
+# Need to zplug install and load twice to first load my dotfiles
+# Then install all packages my dotfiles add.
 zplug install
+zplug load
 
-# Then, source plugins and add commands to $PATH
+# Update dotfiles repo
+zplug update
+
+zplug install
 zplug load
